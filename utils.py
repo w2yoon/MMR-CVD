a8949e88b573c526c95219545fe3da12b6cd4287
w2yoon <109725273+w2yoon@users.noreply.github.com>
2025-02-28 17:09:27 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 import torchvision.transforms.functional as TF
 from PIL import Image
 
-def center_crop_square_fundus(img: Image.Image):
+def center_crop_square_cfp(img: Image.Image):
     w, h = img.size 
     crop_size = h 
     return TF.center_crop(img, (crop_size, crop_size))
